aa792403cdbdd88b1e8c33fcb30e1cb9b42f7ada
K. Adam White <kadamwhite@users.noreply.github.com>
2017-02-24 17:12:03 -0500

    step2

---
+++ image-annotator/src/redux/reducers/loading.js
@@ -0,0 +1,27 @@
+import { REQUEST_FACES, RECEIVE_FACES, REQUEST_FACES_FAILED } from '../actions';
+
+const defaultState = {
+  faces: false,
+};
+
+export default function loadingReducer(state = defaultState, action) {
+  if (action.type === REQUEST_FACES) {
+    return Object.assign({}, state, {
+      faces: true,
+    });
+  }
+
+  if (action.type === RECEIVE_FACES) {
+    return Object.assign({}, state, {
+      faces: false,
+    });
+  }
+
+  if (action.type === REQUEST_FACES_FAILED) {
+    return Object.assign({}, state, {
+      faces: false,
+    });
+  }
+
+  return state;
+}
