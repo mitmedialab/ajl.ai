d60d44b3ef71edb56d3676e8e7ddeca43017771e
Boaz Sender <boaz@bocoup.com>
2017-03-03 12:14:58 -0500

    create percieved demo componants

--- frontend/components/PerceivedDemographics.jsx
+++ frontend/components/PerceivedDemographics.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { requestFaces } from '../redux/actions';
 import CanvasContainer from './CanvasContainer';
 import CarouselContainer from './CarouselContainer';
 
 
-export default () => (
-  <div>
-    <CanvasContainer/>
-    <CarouselContainer/>
-  </div>
-)
+// Kick off initial data load
+// store.dispatch(requestFaces());
+
+
+
+class PerceivedDemographicsContainer extends Component {
+  componentDidMount () {
+    this.props.onEnter();
+  }
+
+  render () {
+    return (
+      <div>
+        <CanvasContainer/>
+        <CarouselContainer/>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = dispatch => ({
+  onEnter: () => dispatch(requestFaces()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PerceivedDemographicsContainer);
