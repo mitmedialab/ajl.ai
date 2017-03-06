cf12fd65985ab3219bd690820228ab9d2aa9f08d
K. Adam White <kadamwhite@users.noreply.github.com>
2017-03-06 17:51:13 -0500

    Remove inspecific doc comments in actions file

--- frontend/redux/actions.js
+++ frontend/redux/actions.js
@@ -11,7 +11,6 @@
 
 /** @prop {string} Action to select the active face */
 export const SELECT_FACE = 'SELECT_FACE';
-/** SELECT_FACE action creator */
 export const selectFace = image => ({
   type: SELECT_FACE,
   payload: image,
@@ -19,21 +18,18 @@ export const selectFace = image => ({
 
 /** @prop {string} Action to select the next available face */
 export const NEXT_FACE = 'NEXT_FACE';
-/** NEXT_FACE action creator */
 export const nextFace = () => ({
   type: NEXT_FACE,
 });
 
 /** @prop {string} Action to select the previous available face */
 export const PREVIOUS_FACE = 'PREVIOUS_FACE';
-/** PREVIOUS_FACE action creator */
 export const previousFace = () => ({
   type: PREVIOUS_FACE,
 });
 
 /** @prop {string} Action to populate the faces within the store */
 export const SET_FACES = 'SET_FACES';
-/** SET_FACES action creator */
 export const setFaces = faces => ({
   type: SET_FACES,
   payload: faces,
@@ -41,14 +37,13 @@ export const setFaces = faces => ({
 
 /** @prop {string} Action to trigger a request for face data */
 export const REQUEST_FACES = 'REQUEST_FACES';
-/** REQUEST_FACES action creator */
+
 export const requestFaces = () => ({
   type: REQUEST_FACES,
 });
 
 /** @prop {string} Action to populate the faces within the store when the request succeeds */
 export const RECEIVE_FACES = 'RECEIVE_FACES';
-/** RECEIVE_FACES action creator */
 export const receiveFaces = faces => ({
   type: RECEIVE_FACES,
   payload: faces,
@@ -56,7 +51,6 @@ export const receiveFaces = faces => ({
 
 /** @prop {string} Action to signal a data request failed */
 export const REQUEST_FACES_FAILED = 'REQUEST_FACES_FAILED';
-/** REQUEST_FACES_FAILED action creator */
 export const requestFacesFailed = error => ({
   type: REQUEST_FACES_FAILED,
   payload: error,
@@ -65,14 +59,12 @@ export const requestFacesFailed = error => ({
 
 /** @prop {string} Action to trigger a request for face data */
 export const REQUEST_ANNOTATIONS = 'REQUEST_ANNOTATIONS';
-/** REQUEST_ANNOTATIONS action creator */
 export const requestAnnotations = () => ({
   type: REQUEST_ANNOTATIONS,
 });
 
 /** @prop {string} Action to populate the annotations within the store when the request succeeds */
 export const RECEIVE_ANNOTATIONS = 'RECEIVE_ANNOTATIONS';
-/** RECEIVE_ANNOTATIONS action creator */
 export const receiveAnnotations = annotations => ({
   type: RECEIVE_ANNOTATIONS,
   payload: annotations,
@@ -80,7 +72,6 @@ export const receiveAnnotations = annotations => ({
 
 /** @prop {string} Action to signal a data request failed */
 export const REQUEST_ANNOTATIONS_FAILED = 'REQUEST_ANNOTATIONS_FAILED';
-/** REQUEST_ANNOTATIONS_FAILED action creator */
 export const requestAnnotationsFailed = error => ({
   type: REQUEST_ANNOTATIONS_FAILED,
   payload: error,
