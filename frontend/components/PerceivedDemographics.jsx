575f322e0f3d72049f85faa5453df2b24a436d62
Boaz Sender <boaz@bocoup.com>
2017-03-06 11:31:39 -0500

    add working form, console logs demos

--- frontend/components/PerceivedDemographics.jsx
+++ frontend/components/PerceivedDemographics.jsx
@@ -4,6 +4,8 @@ import { requestFaces } from '../redux/actions';
 import CanvasContainer from './CanvasContainer';
 import CarouselContainer from './CarouselContainer';
 
+import styles from './PerceivedDemographics.styl';
+
 
 // Kick off initial data load
 // store.dispatch(requestFaces());
@@ -15,10 +17,75 @@ class PerceivedDemographicsContainer extends Component {
     this.props.onEnter();
   }
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      percievedAge: 0,
+      pervievedGender: '',
+      percievedEthnicity: '',
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+
+    this.setState({
+      [name]: value
+    });
+
+    console.log(value);
+
+  }
+
+  handleSubmit(event) {
+    console.log('form value: ', this.state);
+    event.preventDefault();
+  }
+
   render () {
     return (
       <div>
         <CanvasContainer/>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            age: (younger)
+            <input
+              name="percievedAge"
+              type="range"
+              checked={this.state.percievedAge}
+              onChange={this.handleInputChange}
+              min="0" max="5" />
+            (older)
+          </label>
+
+          <label>
+            percieved  gender:
+            <select name="pervievedGender" value={this.state.pervievedGender} onChange={this.handleInputChange}>
+              <option value="male">male</option>
+              <option  value="female">female</option>
+              <option value="genderqueer">genderqueer</option>
+            </select>
+          </label>
+
+          <label>
+            perceived ethnicity:
+            <select name="percievedEthnicity" value={this.state.percievedEthnicity} onChange={this.handleInputChange}>
+              <option value="black">black</option>
+              <option value="latino">latino</option>
+              <option value="white">white</option>
+              <option value="asian">asian</option>
+              <option value="other">other</option>
+            </select>
+          </label>
+
+          <input type="submit" value="Submit" />
+        </form>
         <CarouselContainer/>
       </div>
     )
