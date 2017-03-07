539fe13f0d98832ea14645308fbf7ced05236921
K. Adam White <kadamwhite@users.noreply.github.com>
2017-03-07 16:33:46 -0500

    Fix typos and slightly adjust copy

--- frontend/components/Home.jsx
+++ frontend/components/Home.jsx
@@ -4,16 +4,18 @@ import { Link } from 'react-router-dom';
 export default () => (
   <div>
     <h1>The Computer Vision Benchmarking Project</h1>
-    <h5>Fromt the Algorithmic Justice League</h5>
-    <h2>Help us measure bias in computer vision systems</h2>
+    <h5>From the Algorithmic Justice League</h5>
+    <h2>Help us to measure bias in computer vision systems</h2>
     <p>
-      The AJL is building the first annotated data set of diverse faces to
-      benchmark bias face recognition systems.
+      The <acronym title="Algorithmic Justice League">AJL</acronym> is
+      building the first annotated data set of diverse faces to benchmark
+      bias in facial recognition systems.
     </p>
     <p>
-      You can help by labeling <Link to="/PerceivedDemographics">perceived demographics</Link>,
+      You can help by labeling images
+      with <Link to="/PerceivedDemographics">perceived demographics</Link>,
       identifying specific <Link to="/Regions">regions</Link> and
-      <Link to="/Landmarks">outlining features</Link> of faces in our curated
+      <Link to="/Landmarks">outlining features</Link> of the faces in our curated
       database of pictures of public domain faces.
     </p>
   </div>
