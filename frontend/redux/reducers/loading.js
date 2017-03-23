16251a722d6a12f8bcdff9121bb6bd3ec19de294
K. Adam White <kadamwhite@users.noreply.github.com>
2017-03-23 17:58:50 -0400

    Needlessly refactor the loading reducer to match the shape of other reducers

--- frontend/redux/reducers/loading.js
+++ frontend/redux/reducers/loading.js
@@ -2,49 +2,48 @@
  * This reducer manages UI flags that indicate whether any AJAX requests are
  * currently in-flight within the application.
  */
+import { combineReducers } from 'redux';
 import {
   REQUEST_ATTRIBUTES, RECEIVE_ATTRIBUTES, REQUEST_ATTRIBUTES_FAILED,
   REQUEST_WORKLOAD, RECEIVE_WORKLOAD, REQUEST_WORKLOAD_FAILED,
   COMPLETE_WORKLOAD, COMPLETE_WORKLOAD_FAILED,
 } from '../actions';
 
-const defaultState = {
-  faces: false,
-  attributes: false,
-};
+// Each of these methods reduces the state property that shares its name
 
-export default function loadingReducer(state = defaultState, action) {
+function attributes(state = false, action) {
   switch (action.type) {
 
   case REQUEST_ATTRIBUTES:
-    return {
-      ...state,
-      attributes: true,
-    };
+    return true;
 
   case RECEIVE_ATTRIBUTES:
   case REQUEST_ATTRIBUTES_FAILED:
-    return {
-      ...state,
-      attributes: false,
-    };
-
-  case REQUEST_WORKLOAD:
-  case COMPLETE_WORKLOAD:
-    return {
-      ...state,
-      workload: true,
-    };
-
-  case RECEIVE_WORKLOAD:
-  case REQUEST_WORKLOAD_FAILED:
-  case COMPLETE_WORKLOAD_FAILED:
-    return {
-      ...state,
-      workload: false,
-    };
+    return false;
 
   default:
     return state;
   }
 }
+
+function workload(state = false, action) {
+  switch (action.type) {
+
+  case REQUEST_WORKLOAD:
+  case COMPLETE_WORKLOAD:
+    return true;
+
+  case RECEIVE_WORKLOAD:
+  case REQUEST_WORKLOAD_FAILED:
+  case COMPLETE_WORKLOAD_FAILED:
+    return false;
+
+  default:
+    return state;
+  }
+}
+
+export default combineReducers({
+  attributes,
+  workload,
+});
