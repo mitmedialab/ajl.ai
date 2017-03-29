148d4bb5f63718b804b0fcf1e93539d2bc6335b5
Corey Frang <gnarf37@gmail.com>
2017-03-29 12:37:00 -0400

    Track workload.submitted_at instead of created_at for annotation

--- backend/db/migrations/j0tx6htl.timestamps.sql
+++ backend/db/migrations/j0tx6htl.timestamps.sql
@@ -1,13 +1,13 @@
 ALTER TABLE workload
  ADD created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP;
 
-ALTER TABLE image_annotation
- ADD created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE workload
+ ADD submitted_at TIMESTAMPTZ;
 
 ---
 
 ALTER TABLE workload
  DROP created_at;
 
-ALTER TABLE image_annotation
- DROP created_at;
+ALTER TABLE workload
+ DROP submitted_at;
--- backend/endpoints/annotations/queries/scoreWorkload.sql
+++ backend/endpoints/annotations/queries/scoreWorkload.sql
@@ -1,1 +1,1 @@
-UPDATE workload SET score = ${score} WHERE id = ${id};
+UPDATE workload SET score = ${score}, submitted_at = CURRENT_TIMESTAMP WHERE id = ${id};
