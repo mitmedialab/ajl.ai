d570a4f4af28dd30a62fb2563f4a3ad122311f5b
Tyler Kellen <tyler@sleekcode.net>
2017-04-03 13:38:05 -0400

    fixup! add support for detecting who is logged in

--- backend/endpoints/index.js
+++ backend/endpoints/index.js
@@ -1,9 +1,11 @@
 import * as healthCheck from './health/routes';
 import * as annotations from './annotations/routes';
 import * as feedback from './feedback/routes';
+import * as feedback from './session/routes';
 
 export default [
   healthCheck,
   annotations,
   feedback,
+  session,
 ];
