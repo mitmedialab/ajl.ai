41f154839f49a81b7bc16495873b3438bf11bda8
Tyler Kellen <tyler@sleekcode.net>
2017-04-04 11:30:02 -0400

    add note per @gnarf

--- backend/endpoints/session/routes.js
+++ backend/endpoints/session/routes.js
@@ -1,5 +1,9 @@
 export const location = '/api/session';
 export const routes = {
+  // this route is in place purely to let the front end be aware of who
+  // is logged in via basic auth, to pre-fill a feedback form. if we are
+  // out of private beta and you are reading this, it is likely we no
+  // longer need the `/me` route.
   get: {
     '/me': (req, res) => {
       res.send({
