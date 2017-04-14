101941142bfd73d78587a634216f85fa972d1f32
Boaz Sender <boaz@bocoup.com>
2017-04-14 18:22:03 -0400

    new IRB protocol

--- frontend/components/Overlays/AgeConsentModal.jsx
+++ frontend/components/Overlays/AgeConsentModal.jsx
@@ -4,11 +4,17 @@ import Modal from './Modal';
 const AgeConsentModal = ({ onConfirmAgeConsent, onRejectAgeConsent }) => (
   <Modal
     onConfirm={onConfirmAgeConsent}
-    confirmText="I am 18 or Over"
+    confirmText="I Agree"
     onCancel={onRejectAgeConsent}
-    cancelText="No"
+    cancelText="I Disagree"
   >
-    <h1>Are you over the age of 18?</h1>
+    <h1>Consent Form</h1>
+    <p>
+      AJL.AI is part of an ongoing MIT scientific research project. Your decision to participate in annotating images is voluntary. We do not collect personally identifiable data with your annotations. The data we will have, in addition to your responses, is the time at which you completed annotating an image.
+    </p>
+    <p>
+      The results of the research may be presented at scientific meetings or published in scientific journals. Selecting the 'I Agree' button indicates that you are at least 18 years of age and agree to annotate voluntarily.
+    </p>
   </Modal>
 );
 
