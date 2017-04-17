35916c4b8612f6aff94e8eae30bdc5d9a4084faa
Mx Corey Frang <gnarf37@gmail.com>
2017-04-17 12:21:41 -0400

    Small tweaks to csv report (#215)
    * Add submitted_at to the CSV report
    
    * order by id instead, should look more natural
    
    * fix DROP VIEW IF EXISTS

--- backend/db/migrations/j1mbinbm.report-add-submit-timestamp.sql
+++ backend/db/migrations/j1mbinbm.report-add-submit-timestamp.sql
@@ -1,3 +1,4 @@
+DROP VIEW IF EXISTS image_annotation_report;
 CREATE OR REPLACE VIEW image_annotation_report AS
   SELECT
     ia.id,
@@ -6,10 +7,13 @@ CREATE OR REPLACE VIEW image_annotation_report AS
     ia.image_id,
     i.url,
     aa.name AS attribute,
-    ao.name AS value
+    ao.name AS value,
+    w.submitted_at
   FROM image_annotation ia
     LEFT JOIN image i ON ia.image_id = i.id
     LEFT JOIN annotation_option ao ON ia.annotation_option_id = ao.id
-    LEFT JOIN annotation_attribute aa ON ao.annotation_attribute_id = aa.id;
+    LEFT JOIN annotation_attribute aa ON ao.annotation_attribute_id = aa.id
+    LEFT JOIN workload w ON ia.workload_id = w.id
+  ORDER BY ia.id;
 ---
 DROP VIEW IF EXISTS image_annotation_report;
