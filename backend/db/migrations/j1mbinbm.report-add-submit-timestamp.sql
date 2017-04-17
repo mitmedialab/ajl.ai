b4ebc53e344c50a56a4ed0670adc2db4ca4b5c9d
Corey Frang <gnarf37@gmail.com>
2017-04-17 12:13:19 -0400

    Add submitted_at to the CSV report

---
+++ backend/db/migrations/j1mbinbm.report-add-submit-timestamp.sql
@@ -0,0 +1,19 @@
+DROP VIEW image_annotation_report;
+CREATE OR REPLACE VIEW image_annotation_report AS
+  SELECT
+    ia.id,
+    ia.annotator_id,
+    ia.workload_id,
+    ia.image_id,
+    i.url,
+    aa.name AS attribute,
+    ao.name AS value,
+    w.submitted_at
+  FROM image_annotation ia
+    LEFT JOIN image i ON ia.image_id = i.id
+    LEFT JOIN annotation_option ao ON ia.annotation_option_id = ao.id
+    LEFT JOIN annotation_attribute aa ON ao.annotation_attribute_id = aa.id
+    LEFT JOIN workload w ON ia.workload_id = w.id
+  ORDER BY submitted_at;
+---
+DROP VIEW image_annotation_report;
