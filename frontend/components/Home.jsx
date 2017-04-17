96c7dbb33f3471bdd14ef74c33a703c3a6ae93c3
Boaz Sender <boaz@bocoup.com>
2017-04-17 00:02:09 -0400

    further home page and FAQ copy edits

--- frontend/components/Home.jsx
+++ frontend/components/Home.jsx
@@ -10,7 +10,7 @@ export default () => (
     <div className={styles.mainContainer}>
       <div className={styles.hompageContainer}>
         <div className={styles.homeSubHeader}>
-          <h1>Destiny Unveiled: Facial Data Transparency Project</h1>
+          <h1>Facial Data Transparency Project</h1>
           <h5>
             From the <a href="http://ajlunited.org">
               Algorithmic Justice League
@@ -18,40 +18,11 @@ export default () => (
           </h5>
         </div>
         <section className={styles.homepageProgressBar}>
-          <b>
-            If data is destiny, how inclusive is our future data-centric
-            technology?
-          </b> How representative are the datasets being used to train machines
-          to analyze faces? Help us find out by tagging training images.
+          <p>
+            Help us check for bias by tagging pictures of celebrities with demographic labels.
+          </p>
           <KnownOverallProgressContainer />
           <Link className={styles.homepageButton} to="annotate">Start Tagging</Link>
-        </section>
-        <section>
-          <h2>Make a Difference</h2>
-          <p>
-            Algorithms are trained and tested with data sets.
-            A lack of diversity in these data sets can lead
-            to biased systems. Help us check for bias by tagging
-            influential data sets.
-          </p>
-        </section>
-        <section>
-          <h2>Simple Actions Matter</h2>
-          <p>
-            The simple action of tagging face images with perceived
-            age, gender, and ethnicity will generate bias busting
-            information that will inform the creation of inclusive technology.
-          </p>
-        </section>
-        <section>
-          <h2>Be Heard</h2>
-          <p>
-            As algorithms increasingly influence more aspects of our lives,
-            we need ways to keep coded systems accountable.
-            Please <a className={styles.homepageContactUs} href="https://goo.gl/forms/jBwH8fuMqZwLf6Mr2">be in touch</a> with
-            questions, comments, concerns, and experiences relating
-            to algorithmic bias.
-          </p>
         </section>
       </div>
     </div>
