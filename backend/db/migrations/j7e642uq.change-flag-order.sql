2b7bc7fa36c4a8f9b7dc2ef2395bfd598ba911f6
joyab <joyab@mit.edu>
2017-09-23 16:29:38 -0400

    Updated the order of listed ethnicities

--- backend/db/migrations/j7e642uq.change-flag-order.sql
+++ backend/db/migrations/j7e642uq.change-flag-order.sql
@@ -41,3 +41,33 @@ UPDATE annotation_option
 UPDATE annotation_option
   SET name = 'No Subject in Picture'
   WHERE name = 'No Subject In Image';
+
+
+
+UPDATE annotation_option
+  SET sort_order = 1
+  WHERE name = 'Asian';
+
+UPDATE annotation_option
+  SET sort_order = 2
+  WHERE name = 'South Asian';
+
+UPDATE annotation_option
+  SET sort_order = 3
+  WHERE name = 'Black';
+
+UPDATE annotation_option
+  SET sort_order = 4
+  WHERE name = 'White';
+
+UPDATE annotation_option
+  SET sort_order = 5
+  WHERE name = 'Hispanic';
+
+UPDATE annotation_option
+  SET sort_order = 6
+  WHERE name = 'Middle Eastern';
+
+UPDATE annotation_option
+  SET sort_order = 7
+  WHERE name = 'Not Listed' AND annotation_attribute_id = 3;
---
+++ backend/db/migrations/j7xrekby.ethnicity-order.sql
@@ -0,0 +1,50 @@
+UPDATE annotation_option
+  SET sort_order = 1
+  WHERE name = 'Asian';
+
+UPDATE annotation_option
+  SET sort_order = 2
+  WHERE name = 'South Asian';
+
+UPDATE annotation_option
+  SET sort_order = 3
+  WHERE name = 'Black';
+
+UPDATE annotation_option
+  SET sort_order = 4
+  WHERE name = 'White';
+
+UPDATE annotation_option
+  SET sort_order = 5
+  WHERE name = 'Hispanic';
+
+UPDATE annotation_option
+  SET sort_order = 6
+  WHERE name = 'Middle Eastern';
+
+---
+
+UPDATE annotation_option
+  SET sort_order = 1
+  WHERE name = 'Asian';
+
+UPDATE annotation_option
+  SET sort_order = 7
+  WHERE name = 'South Asian';
+
+UPDATE annotation_option
+  SET sort_order = 2
+  WHERE name = 'Black';
+
+UPDATE annotation_option
+  SET sort_order = 5
+  WHERE name = 'White';
+
+UPDATE annotation_option
+  SET sort_order = 3
+  WHERE name = 'Hispanic';
+
+UPDATE annotation_option
+  SET sort_order = 4
+  WHERE name = 'Middle Eastern';
+
